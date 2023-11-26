d9f2fdf2539eb336031aff4736be962d63472744
claudiofsn <craudiofelix@gmail.com>
2023-11-26 13:22:41 -0300

    Projeto concluido

---
+++ src/components/Post.tsx
@@ -0,0 +1,145 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+import { useState, FormEvent, ChangeEvent, InvalidEvent } from 'react';
+
+interface Author {
+    name: string
+    role: string
+    avatarUrl: string
+}
+
+interface Content {
+    content: string
+    type: 'paragraph' | 'link'
+}
+
+export interface PostType {
+    id: number
+    author: Author
+    publishedAt: Date
+    content: Content[]
+}
+
+interface PostProps {
+    post: PostType
+}
+
+export function Post({ post }: PostProps) {
+    const [comments, setComments] = useState([
+        'Post muito bacana, hein!?'
+    ]);
+
+    const [newCommentText, setNewCommentText] = useState('');
+
+    const publishedAtDateFormatted = format(post.publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
+    function handleCreateNewComment(event: FormEvent) {
+        event.preventDefault();
+
+        setComments([...comments, newCommentText]);
+
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity("");
+        setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity("Esse campo é obrigatório")
+    }
+
+    function deleteComment(commentToDelete: string) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete
+        })
+
+        setComments(commentsWithoutDeletedOne)
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
+
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar
+                        src={post.author.avatarUrl}
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>{post.author.name}</strong>
+                        <span>{post.author.role}</span>
+                    </div>
+                </div>
+                <time
+                    title={publishedAtDateFormatted}
+                    dateTime={post.publishedAt.toISOString()}
+                >
+                    {publishedDateRelativeToNow}
+                </time>
+            </header>
+
+            <div className={styles.content}>
+                {post.content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>
+                    }
+
+                    if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }
+                }
+                )}
+            </div>
+
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    name="comment"
+                    placeholder='Deixe um comentário'
+                    required
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                />
+
+                <footer>
+                    <button
+                        type='submit'
+                        disabled={isNewCommentEmpty}
+                    >
+                        Publicar
+                    </button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                {comments.map((comment) =>
+                    <Comment
+                        key={comment}
+                        content={comment}
+                        onDeleteComment={deleteComment}
+                    />
+                )}
+            </div>
+        </article>
+    )
+}
+
+/*
+Default Exports = com essa exportação, você pode alterar o nome do componente no arquivo onde ele é importante
+*/
+
+// Named Exports = dessa forma você deve desestruturar o import no arquivo onde é importado
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1699727152109-b5b9592641ca?q=85&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Imagem de capa do perfil do usuário"
+            />
+
+            <div className={styles.profile}>
+
+                <Avatar
+                    src="https://github.com/claudiofsn.png"
+                />
+
+                <strong>Claudio Neto</strong>
+                <span>Full Stack Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine
+                        size={20}
+                    />
+                    Editar Perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
